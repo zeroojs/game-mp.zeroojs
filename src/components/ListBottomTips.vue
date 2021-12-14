8d119e9f2cb7da2b811efac3831c6cc9c906b3e8
smallMote <luckyoung@certbase.com>
2021-12-14 11:38:03 +0800

    add:列表底部提示组件

---
+++ src/components/ListBottomTips.vue
@@ -0,0 +1,29 @@
+<template>
+  <view>
+    <view v-if="loading" class="nomore-tips">加载中...</view>
+    <view v-else class="nomore-tips">亲，已经到底了~</view>
+  </view>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    loading: Boolean
+  },
+  setup() {
+    return {
+    }
+  }
+})
+</script>
+
+<style lang="less" scoped>
+.nomore-tips {
+  text-align: center;
+  font-size: 26upx;
+  color: #999;
+  padding: 20upx 0;
+}
+</style>
